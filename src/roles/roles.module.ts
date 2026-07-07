84032c9918962af499cac2fdeb00992e9b4a4746
Dragon <167077773+Dragon-Palestine@users.noreply.github.com>
2026-07-07 14:24:16 +0300

    refactor: clean up roles and add RolePermission provider

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -6,10 +6,12 @@ import { Role } from './entitys/role.entity';
 import { RolePermission } from './entitys/role-permission.entity';
 import { Permission } from 'src/permission/permission.entity';
 
+import { RolePermissionProvider } from './role-permission.provider';
+
 @Module({
   controllers: [RoleController],
-  providers: [RoleService],
+  providers: [RoleService, RolePermissionProvider],
   imports: [TypeOrmModule.forFeature([Role, RolePermission, Permission])],
-  exports: [TypeOrmModule],
+  exports: [TypeOrmModule, RolePermissionProvider],
 })
 export class RolesModule {}
